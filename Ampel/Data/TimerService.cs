4fd0d26db7929483d5cc9f46f37bb5ce4fc03666
HannesPreishuber <hannesp@ppedv.de>
2021-03-25 21:27:31 +0100

    Projektdateien hinzufügen.

---
+++ Ampel/Data/TimerService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+namespace Ampel.Data
+{
+    public class TimerService
+    {
+
+        public TimerService()
+        {
+            _timer = new Timer();
+            _timer.Elapsed += NotifyTimerElapsed;
+            _timer.Enabled = true;
+            _timer.AutoReset = false;
+        }
+        private Timer _timer;
+
+        public void SetTimer(double interval)
+        {
+            _timer.Interval = interval;
+            _timer.Start();
+        }
+
+        public event Action OnElapsed;
+
+        private void NotifyTimerElapsed(Object source, ElapsedEventArgs e)
+        {
+            OnElapsed?.Invoke();
+        
+        }
+    }
+}
